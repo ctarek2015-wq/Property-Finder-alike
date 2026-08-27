bfb9c3c3e7496e77cfae14d1fd7220c342125ce4
ahmed-tarek <c.tarek2015@gmail.com>
2026-08-27 11:24:09 +0300

    refactor: disable email notifications in appointment and auth controllers due to deployment issues

--- controllers/appointmentCtrl.js
+++ controllers/appointmentCtrl.js
@@ -51,23 +51,23 @@ const create = async (req, res) => {
     newViewing.propertyId = req.query.propertyId;
     newViewing.ownerId = prop.owner._id;
     await newViewing.save();
-
-    try {
-      const owner = await User.findById(prop.owner);
-      if (owner) {
-        await sendAppointmentNotification({
-          owner,
-          seeker: req.session.user,
-          property: prop,
-          viewing: newViewing,
-        });
-      }
-    } catch (emailError) {
-      console.log(
-        "Booking notification email could not be sent:",
-        emailError.message,
-      );
-    }
+    // disabled email notification for now, as it was causing issues with the deployed app
+    // try {
+    //   const owner = await User.findById(prop.owner);
+    //   if (owner) {
+    //     await sendAppointmentNotification({
+    //       owner,
+    //       seeker: req.session.user,
+    //       property: prop,
+    //       viewing: newViewing,
+    //     });
+    //   }
+    // } catch (emailError) {
+    //   console.log(
+    //     "Booking notification email could not be sent:",
+    //     emailError.message,
+    //   );
+    // }
 
     res.redirect(`/appointments`);
   } catch (err) {
--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -110,12 +110,13 @@ const register = async (req, res) => {
 
     // Create user and session
     const createUser = await User.create(req.body);
+    // disabled email notification for now, as it was causing issues with the deployed app
 
-    try {
-      await sendWelcomeEmail(createUser);
-    } catch (emailError) {
-      console.log("Welcome email could not be sent:", emailError.message);
-    }
+    // try {
+    //   await sendWelcomeEmail(createUser);
+    // } catch (emailError) {
+    //   console.log("Welcome email could not be sent:", emailError.message);
+    // }
     req.session.user = {
       username: createUser.username,
       role: createUser.role,
